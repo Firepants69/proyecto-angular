b5cc56614829ceb9b81de59b8c48b36f60db2f1a
Hector-Adrian <hectoradrianmateuarellano@gmail.com>
2024-12-11 23:43:14 -0600

    Ahora si, editar perfil pero no hace nada

---
+++ src/app/edit-profile/edit-profile.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from './../core/services/account.service';
+
+@Component({
+  selector: 'app-edit-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-profile.component.html',
+  styleUrl: './edit-profile.component.css'
+})
+export class EditProfileComponent implements OnInit{
+  constructor(public accountService: AccountService){
+
+  }
+
+  ngOnInit(): void {
+    this.getProfile();
+  }
+
+  getProfile(){
+    this.accountService.getAccount().subscribe({
+      next: (data)=>{
+        console.log(data)
+        this.accountService.account = {...data,date:new Date(data.date).toLocaleDateString()};
+      },error: (err)=>{
+        console.error(err);
+      }
+    })
+  }
+
+  
+}
