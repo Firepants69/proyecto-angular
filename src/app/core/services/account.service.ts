ccd42e86613c43446291d8147282b3ca47596342
¨Rafael <rafaelmerlinprieto@gmail.com>
2024-12-12 03:15:00 -0600

    merge completo

--- src/app/core/services/account.service.ts
+++ src/app/core/services/account.service.ts
@@ -1,68 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { AccountService } from '../../core/services/account.service';
-import { BoardsService } from '../../core/services/boards.service';
-import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { Injectable } from '@angular/core';
+import { baseUrl } from '../../../baseUrl';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-@Component({
-  selector: 'app-profile',
-  standalone: true,
-  imports: [CommonModule, RouterModule],
-  templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+@Injectable({
+  providedIn: 'root'
 })
-export default class ProfileComponent implements OnInit{
-  constructor(public accountService: AccountService,public boardsService: BoardsService){
-
+export class AccountService {
+  readonly API_URL = `${baseUrl}/Users`
+  readonly YOUR_BOARDS = `${baseUrl}/Boards/your-boards`
+  account: any;
+  your_board: any[]
+  constructor(private http: HttpClient) { 
+    this.account = {}
+    this.your_board = []
   }
-
-  ngOnInit(): void {
-    this.getProfile();
-    this.getYourBoards();
+  getAccount(){
+    const token = localStorage.getItem('authToken');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+    return this.http.get<any>(this.API_URL,{headers});
   }
-
   getYourBoards(){
-    this.accountService.getYourBoards().subscribe({
-      next:(data=>{
-        this.accountService.your_board = data.map(board=>({
-          Usuario: board.user.name,
-          Texto: board.content,
-          Likes: board.numOfLikes,
-          Comentarios: board.numOfResponses,
-          isLiked : board.isLiked,
-          boardId: board.id,
-          accountImage: board.user.image == null ? "https://localhost:7257/user-ico/default.jpg":board.user.image  
-        }))
-      }),error:(err)=>{
-        console.error(err)
-      }
-    })
-  }
-  toggleLike(board: any,event:Event) {
-    event.stopPropagation();
-    if (board.isLiked) {
-      // Si ya le ha dado like, eliminarlo
-      this.boardsService.removeLike(board.boardId).subscribe(() => {
-        board.Likes--;
-        board.isLiked = false;
-      });
-    } else {
-      // Si no le ha dado like, agregarlo
-      this.boardsService.addLike(board.boardId).subscribe(() => {
-        board.Likes++;
-        board.isLiked = true;
-      });
-    }
+    const token = localStorage.getItem('authToken');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+    return this.http.get<any[]>(this.YOUR_BOARDS,{headers})
   }
 
-  getProfile(){
-    this.accountService.getAccount().subscribe({
-      next: (data)=>{
-        console.log(data)
-        this.accountService.account = {...data,date:new Date(data.date).toLocaleDateString()};
-      },error: (err)=>{
-        console.error(err);
-      }
-    })
+  updateAccount(updatedAccount: any) {
+    const token = localStorage.getItem('authToken');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+    return this.http.put<any>(`${this.API_URL}/${updatedAccount.id}`, updatedAccount, { headers });
   }
+
 }
